ee0892b787ec9b76ced00ac28478ec5ba6c431c2
jlSites <jlsites.git@gmail.com>
2017-05-05 13:22:41 +1000

    can delete a pet by DELETE request for hello-rest

--- hello-rest/index.js
+++ hello-rest/index.js
@@ -5,26 +5,25 @@ var bodyParser = require('body-parser');
 var app = express();
 var fs = require("fs");
 
-// Add headers
+// enable CORS requests
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // parse application/json
 app.use(bodyParser.json());
 
-// list all pets by GET
+// get all pets by GET
 app.get('/pet', function (req, res, next) {
     console.log(">>get=", allPets);
     res.end(JSON.stringify(allPets));
 });
 
-// add/update a pet by POST
+// post(add/update) a pet by POST
 app.post('/pet', function (req, res, next) {
     //console.log(">>req=", req);
     console.log(">>post=req.body=", req.body);
@@ -49,7 +48,7 @@ app.post('/pet', function (req, res, next) {
     res.end(JSON.stringify(postPet));
 });
 
-// get a pet by GET
+// get a pet by name by GET
 app.get('/pet/:name', function (req, res, next) {
     var found;
     for (var i = 0; i < allPets.length; i++) {
@@ -59,14 +58,14 @@ app.get('/pet/:name', function (req, res, next) {
         }
     }
 
-	console.log(">>get=found=", found);
+    console.log(">>get=found=", found);
     res.end(JSON.stringify(found));
 });
 
-// delete a pet by DELETE
+// delete a pet by name by DELETE
 app.delete('/pet/:name', function (req, res, next) {
     var found = -1;
-    for (i = 0; i < allPets.length; i++) {
+    for (var i = 0; i < allPets.length; i++) {
         if (allPets[i].name === req.params.name) {
             found = i;
             break;
