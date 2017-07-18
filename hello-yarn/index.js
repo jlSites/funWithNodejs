92b634ae7ec5ce48fdbd9e089f42f0a2f0efce1f
jlSites <jlsites.git@gmail.com>
2017-07-18 15:48:12 +1000

    played functions in hello-yarn proj

--- hello-yarn/index.js
+++ hello-yarn/index.js
@@ -1,2 +1,26 @@
 var age = 23;
 console.log('my age from ts with yarn=' + age);
+//named function expression
+var nf = function ff(a, b) {
+    return a + b;
+};
+var afHasStatements = function (a, b) { var blah = 33; return a + b; };
+var afOnlyOneStatement = function (a, b) { return a + b; };
+//unnamed function
+var uf = function (a, b) {
+    return a * b;
+};
+var afHasParams = function (a, b) { return a * b; };
+var afNoParams = function () { return 34; };
+//function declaration
+console.log("blahme()=" + blahme(11, 22));
+function blahme(x, y) {
+    return uf(x, y) + nf(x, y);
+}
+//arrow function are always unnamed
+var a = ['blah', 'me', 'she'];
+var ufCalled = a.map(function (s) { return s.length; });
+console.log(ufCalled);
+var afCalled = a.map(function (s) { return s.length + 100; });
+console.log(afCalled);
+console.log("aa=" + a.filter(function (s) { return s.length >= 3; }));
--- hello-yarn/index.ts
+++ hello-yarn/index.ts
@@ -1,3 +1,34 @@
-let age = 23;
 
+let age = 23;
 console.log('my age from ts with yarn=' + age);
+
+//named function expression
+let nf = function ff(a, b) {
+    return a + b;
+}
+let afHasStatements = (a, b) => { let blah = 33; return a + b; };
+let afOnlyOneStatement = (a, b) => a + b;
+
+//unnamed function
+let uf = function (a, b) {
+    return a * b;
+}
+let afHasParams = (a, b) => a * b;
+let afNoParams = () => 34;
+
+//function declaration
+console.log("blahme()=" + blahme(11, 22));
+function blahme(x, y) {
+    return uf(x, y) + nf(x, y);
+}
+
+//arrow function are always unnamed
+let a = ['blah', 'me', 'she'];
+let ufCalled = a.map(function (s) { return s.length; });
+console.log(ufCalled);
+
+let afCalled = a.map(s => s.length + 100);
+console.log(afCalled);
+
+console.log("aa=" + a.filter(s => s.length >= 3));
+
